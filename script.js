358e0094aa8df92339600f59ba79098a7c0d36bd
jdinh11 <jaden.dinh02@gmail.com>
2024-05-13 12:10:01 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,27 @@
 //import React from 'react'
 
 
-function loadDebtAPI() {
-    var debt = fetch("https://api.fiscaldata.treasury.gov/services/api/fiscal_service/v2/accounting/od/debt_to_penny")
-    debtData = debt.json();
+async function loadDebtAPI() {
+    var debt = await fetch("https://api.fiscaldata.treasury.gov/services/api/fiscal_service/v2/accounting/od/debt_to_penny")
+    debtData = await debt.json();
     console.log(debtData);
 }
 
-function loadFinancialReport() {
-    var fin = fetch("https://api.fiscaldata.treasury.gov/services/api/fiscal_service/v2/accounting/od/statement_net_cost") //need endpoint
-    finData = fin.json();
+async function loadFinancialReport() {
+    var fin = await fetch("https://api.fiscaldata.treasury.gov/services/api/fiscal_service/v2/accounting/od/statement_net_cost") //need endpoint
+    finData = await fin.json();
     console.log(finData);
 }
 
 
-function loadTreasuryAPI() {
-    var tre = fetch("https://api.fiscaldata.treasury.gov/services/api/fiscal_service/v1/accounting/mts/mts_table_1") //need endpoint
-    treData = tre.json(); 
+async function loadTreasuryAPI() {
+    var tre = await fetch("https://api.fiscaldata.treasury.gov/services/api/fiscal_service/v1/accounting/mts/mts_table_1") //need endpoint
+    treData = await tre.json(); 
     console.log(treData); 
 }
 
 async function displayDebt() {
 
 }
+
+window.onload = loadDebtAPI(), loadFinancialReport(), loadTreasuryAPI()
