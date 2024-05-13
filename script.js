26cda39a8fe80916b5af96646273f2ce8c4f6f65
jdinh11 <jaden.dinh02@gmail.com>
2024-05-13 12:21:51 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,20 @@
 //import React from 'react'
 
-
+// DEBT
 async function loadDebtAPI() {
     var debt = await fetch("https://api.fiscaldata.treasury.gov/services/api/fiscal_service/v2/accounting/od/debt_to_penny")
     debtData = await debt.json();
     console.log(debtData);
 }
 
+// SPENDING
 async function loadFinancialReport() {
     var fin = await fetch("https://api.fiscaldata.treasury.gov/services/api/fiscal_service/v2/accounting/od/statement_net_cost") //need endpoint
     finData = await fin.json();
     console.log(finData);
 }
 
-
+// REVENUE
 async function loadTreasuryAPI() {
     var tre = await fetch("https://api.fiscaldata.treasury.gov/services/api/fiscal_service/v1/accounting/mts/mts_table_1") //need endpoint
     treData = await tre.json(); 
