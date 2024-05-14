863b50579530bb1b1b51d07862bc2390fc01252b
jdinh11 <jaden.dinh02@gmail.com>
2024-05-14 12:09:47 -0400

    Updated

--- script.js
+++ script.js
@@ -9,10 +9,12 @@ async function loadDebtAPI() {
 
 // SPENDING
 async function loadFinancialReport() {
-    // let year = document.getElementById("year").value;
+    let year = document.getElementById("year").value;
     var fin = await fetch(`https://api.fiscaldata.treasury.gov/services/api/fiscal_service/v2/accounting/od/statement_net_cost?filter=record_fiscal_year:eq:2023`) //need endpoint
     // does not contain year 2024
     finData = await fin.json();
+
+    let select = document.createElement('select');
 
     finData.data.forEach((res) => {
 
@@ -21,8 +23,9 @@ async function loadFinancialReport() {
         let agencyDropdown = document.getElementById("agency");
 
         let agencyOption = document.createElement("option");
-
-        if (agencyDropdown === null || optionDoesNotExist(agencyDropdown, agency)) {
+        
+        let ignore = ['Subtotal', 'Total', 'Unmatched transactions and balances (Note 1.T)'];
+        if (ignore.indexOf(agency) === -1 && (agencyDropdown === null || optionDoesNotExist(agencyDropdown, agency))) {
             agencyOption.value = agencyOption.text = agency;
             console.log(agencyOption)
             agencyDropdown.appendChild(agencyOption);
@@ -52,5 +55,3 @@ function optionDoesNotExist(selectElement, optionValue) {
     }
     return true; // Option does not exist
   }
-
-window.onload = loadDebtAPI(), loadFinancialReport(), loadTreasuryAPI()
